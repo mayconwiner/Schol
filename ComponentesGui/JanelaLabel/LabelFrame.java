1ccbfbeea308275925ea7d05ae5c3d25b2c1aa2c
Maycon Douglas <mayconwiner@gmail.com>
2017-02-15 17:39:33 -0300

    finalizado Jlabel

--- ComponentesGui/JanelaLabel/LabelFrame.java
+++ ComponentesGui/JanelaLabel/LabelFrame.java
@@ -12,6 +12,7 @@ public class LabelFrame extends JFrame {
 	private JLabel label1;
 	private JLabel label2;
 	private JLabel label3;
+	private JLabel label4;
 
 	public LabelFrame() {
 		super("Testando JLabel");
@@ -21,8 +22,8 @@ public class LabelFrame extends JFrame {
 		label1 = new JLabel("Label com texto");
 		label1.setToolTipText("Isso é uma labe l");
 		add(label1);
-
-		// Construtor JLabel com string icon e argumentos de alinhamento
+		
+				// Construtor JLabel com string icon e argumentos de alinhamento
 		Icon bug = new ImageIcon(getClass().getResource("bug.png"));
 		label2 = new JLabel("Label com texto e icone", bug, SwingConstants.LEFT);
 		label2.setToolTipText("esse é o label 2 ");
@@ -34,6 +35,10 @@ public class LabelFrame extends JFrame {
 		label3.setHorizontalTextPosition(SwingConstants.CENTER);
 		label3.setVerticalTextPosition(SwingConstants.BOTTOM);
 		label3.setToolTipText("isso é o label 3 ");
+		
+		label4 = new JLabel("Label teste para uso do git");
+		label4.setToolTipText("Label para teste");
+		add(label4);
 
 	}
 
